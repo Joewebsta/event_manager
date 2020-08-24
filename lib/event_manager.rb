c85efdabc3bc920ab7e7ae8b28b4d1bd0fc2c8a6
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-24 16:44:17 -0400

    Create personal letters using ERB

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,23 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-
-def create_personal_letter
-  contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
-
-  contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zipcode(zipcode)
-    puts personal_letter(name, legislators)
-  end
-end
-
-def personal_letter(name, legislators)
-  template_letter = File.read 'form_letter.html'
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-  personal_letter
-end
+require 'erb'
 
 def first_name(row)
   row[:first_name]
@@ -36,9 +19,22 @@ def legislators_by_zipcode(zipcode)
       address: zipcode,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    ).officials.map(&:name).join(', ')
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def create_personal_letter
+  contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  template_letter = File.read 'form_letter.erb'
+  erb_template = ERB.new template_letter
+
+  contents.each do |row|
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
+    puts form_letter = erb_template.result(binding)
   end
 end
 
