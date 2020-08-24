bd04579e97a53d420eadea58f5e19eb6e0390816
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-24 19:52:32 -0400

    Create a hash of the registration hours.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -22,6 +22,29 @@ def clean_telephone(telephone)
   puts phone_num.empty? ? '' : phone_num.insert(7, '-').insert(3, '-')
 end
 
+def target_registration_time(contents)
+  hours = contents.map do |row|
+    row[:reg_date].split(' ')[1].split(':')[0].to_i
+  end.sort
+
+  hour_count = hours.each_with_object({}) do |hour, hash|
+    hash[hour] ? hash[hour] += 1 : hash[hour] = 1
+  end
+
+  pp hour_count
+
+  # date = date_time.split(' ')[0]
+  # year = date.split('/')[2].prepend('20').to_i
+  # month = date.split('/')[0].to_i
+  # day = date.split('/')[1].to_i
+
+  # time = date_time.split(' ')[1]
+  # hour = time.split(':')[0].to_i
+  # min = time.split(':')[1].to_i
+
+  # DateTime.new(year, month, day, hour, min)
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -51,8 +74,10 @@ def create_personal_letter
   template_letter = File.read 'form_letter.erb'
   erb_template = ERB.new template_letter
 
+  target_registration_time(contents)
+
   contents.each do |row|
-    clean_telephone(row[:home_phone])
+    # clean_telephone(row[:home_phone])
     # id = row[0]
     # name = row[:first_name]
     # zipcode = clean_zipcode(row[:zipcode])
